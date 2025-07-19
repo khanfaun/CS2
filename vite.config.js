ab510867112f3535d2aa024de88f67a2a88de0cc
khanfaun <khangphan1612@gmail.com>
2025-07-20 02:22:45 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/CS2/',  // ví dụ repo tên "cs2-fresh" thì base: '/cs2-fresh/'
-  plugins: [react()],
-})
